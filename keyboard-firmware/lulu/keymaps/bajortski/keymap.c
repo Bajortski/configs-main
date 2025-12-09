9de1286f01bcd3b183fc383a6b7c2f87bca51150
Bajortski <65676366+Bajortski@users.noreply.github.com>
2025-12-09 12:14:26 -0500

    update lulu keymaps

--- keyboard-firmware/lulu/keymaps/bajortski/keymap.c
+++ keyboard-firmware/lulu/keymaps/bajortski/keymap.c
@@ -25,48 +25,48 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |   :  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LCtrl| LGUI |LAlt  | /BackSP /       \Space \  |  MO2 |  TO2 | MO1  |
+ *                   | LCtrl| LGUI |LAlt  | /BackSP /       \Space \  |  MO2 |  MO1 | TO2  |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 
  [_QWERTY] = LAYOUT(
-  KC_TILD,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
+  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQUAL,
-  KC_ESC, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT, KC_ENT,
+  KC_ESC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT, KC_ENT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_SCLN,
-                          KC_LCTL,    KC_LGUI, KC_LALT, KC_BSPC,  KC_SPC,   MO(_RAISE),   TO(_RAISE), MO(_LOWER)
+                          KC_LCTL,    KC_LGUI, KC_LALT, KC_BSPC,  KC_SPC,   MO(_RAISE),   MO(_LOWER), TO(_RAISE)
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
+ * | SLEP |      |      |      |      |      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  | F10  | F11  | F12  |
+ * |      |      |      |      |      |      |                    |      |      |      |      | MW U |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |   `  |   !  |   @  |   #  |   $  |   %  |-------.    ,-------|   ^  |   &  |   *  |   (  |   )  |   -  |
+ * | BR + |      | MS 3 | MS 2 | MS 1 |      |-------.    ,-------|MSLEFT|MSDOWN| MSUP |MSRGHT| MW D |      |
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------|    |-------|      |      |      |      |      |      |
+ * | BR - |      |      |      |      |      |-------|    |-------|      |      |      |      |      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   |      |      |      | /       /       \      \  |      |      |      |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 [_LOWER] = LAYOUT(
-  _______, _______, _______, _______, _______, _______,                   _______, _______, _______,_______, _______, _______,
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-  KC_GRV, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                    KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  KC_SLEP, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, MS_WHLU, _______,
+  KC_BRIU, _______, MS_BTN3, MS_BTN2, MS_BTN1, _______,                   MS_LEFT, MS_DOWN, MS_UP,   MS_RGHT, MS_WHLD, _______,
+  KC_BRID, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
                              _______, _______, _______, _______, _______,  _______, _______, _______
 ),
 /* RAISE
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * | SLEP |      |      |      |      |      |                    |      |      |      |      |      |      |
+ * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  | F10  | F11  | F12  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |      |      |      |      |                    |MSLEFT|MSDOWN| MSUP |MSRGHT| MW U |      |
+ * |      |      |      |      |      |      |                    |      |      |      |      |      | \ |  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | BR + |      | MS3  | MS2  | MS1  | FIND |-------.    ,-------| Left | Down |  Up  |Right | MW D |      |
+ * |      |      |      |      |      | FIND |-------.    ,-------| Left | Down |  Up  |Right |      |      |
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
- * | BR - | UNDO | CUT | COPY | PASTE |      |-------|    |-------|      |      |      | VOLx | VOL- | VOL+ |
+ * |      | UNDO | CUT | COPY | PASTE |      |-------|    |-------|      |      |      | VOLx | VOL- | VOL+ |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   |      |      |      | /       /       \      \  |      |      |      |
  *                   |      |      |      |/       /         \      \ |      |      |      |
@@ -74,10 +74,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_RAISE] = LAYOUT(
-  KC_SLEP, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______,                     MS_LEFT, MS_DOWN, MS_UP,   MS_RGHT, MS_WHLU, _______,
-  KC_BRIU, _______, MS_BTN3, MS_BTN2, MS_BTN1, KC_FIND,                     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, MS_WHLD, _______,
-  KC_BRID, KC_UNDO, KC_CUT,  KC_COPY, KC_PSTE, _______, _______, _______,   _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU,
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+  _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, KC_BSLS,
+  _______, _______, _______, _______, _______, KC_FIND,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
+  _______, KC_UNDO, KC_CUT,  KC_COPY, KC_PSTE, _______, _______, _______, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU,
                              _______, _______, _______,  _______, _______,  _______, _______, _______
 ),
 /* ADJUST
@@ -97,8 +97,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_ADJUST] = LAYOUT(
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  _______, _______, _______, _______, _______, _______,                   _______, _______, _______,_______, _______, _______,
                              _______, _______, _______, _______, _______,  _______, _______, _______
   )
 };
